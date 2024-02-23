bb68a78061751d566adbbbccd93fce70192ae96d
Sagar Pawar <sagar.pawar.dev@gmail.com>
2024-02-23 23:43:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var msgs = [
+  "Hello I am Sagar",
+  "I Created this this Animation :)"
+];
+
+var msgIdx = -1;
+function nextMsg (){
+  msgIdx = (msgIdx+1)%msgs.length;
+  return Array.from(msgs[msgIdx]);
+}
+
+var stkVisible = [];
+var stkInvisible = nextMsg().reverse();
+var forward = true;
+
+function typeNextIn(time){
+  var txt = document.getElementById("txt");
+  txt.innerHTML = stkVisible.join("");
+  setTimeout( ()=>{
+    if(forward){
+      stkVisible.push(stkInvisible.pop());
+      if(stkInvisible.length === 0){
+        typeNextIn(800);
+        forward = false;
+      }
+      else{
+        typeNextIn(250); 
+      }
+    }
+    else{
+      stkInvisible.push(stkVisible.pop());
+      if(stkVisible.length == 0){
+        forward = true;
+        stkInvisible = nextMsg().reverse();
+        typeNextIn(1000);
+      }
+      else{
+        typeNextIn(100); 
+      }
+    }
+  }, time);
+};
+
+function blinkCursor(){
+  setInterval(
+    () => {
+      toggleVisiblity();
+    },
+  350);
+}
+
+function toggleVisiblity(){
+  var cursor  = document.getElementById("cursor");
+      cursor.style.visibility = cursor.style.visibility === 'visible' ? 'hidden': 'visible';
+}
+
+typeNextIn(0);
+blinkCursor();
